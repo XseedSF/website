d17040f1f53beaf87fbbb838cd4d748e0e58bc22
Diego Cáceres <dcaceres@xseed.com.uy>
2017-04-28 13:40:16 -0300

    added new version of page video

--- react-ui/src/components/header/VideoPlayer.js
+++ react-ui/src/components/header/VideoPlayer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from '../../images/xSeedLogo.png';
-import video1 from '../../videos/xseed720.webm';
+import video1 from '../../videos/xseed720.mp4';
 
 const VideoPlayer = () => {
 	return (
@@ -8,7 +8,8 @@ const VideoPlayer = () => {
             <div className="overlay"></div>
            	
 			<video className="video" loop autoPlay muted>
-				<source src={video1} type='video/webm;codecs="vp8, vorbis"' />
+				{ /* <source src={video1} type='video/webm;codecs="vp8, vorbis"' /> */ }
+				<source src={video1} type='video/mp4;codecs="avc1.42E01E, mp4a.40.2"' />
 			    <img src={logo} title="Your browser does not support the <video> tag" />
 			</video>
 

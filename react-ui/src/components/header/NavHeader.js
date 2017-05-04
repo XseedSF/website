67600d3f426896b0e22284063eb3d740e6020f97
Diego Cáceres <dcaceres@xseed.com.uy>
2017-05-04 18:23:27 -0300

    Fixed NavItems in mobile, and added azure start command

--- react-ui/src/components/header/NavHeader.js
+++ react-ui/src/components/header/NavHeader.js
@@ -1,57 +1,50 @@
 import React, { Component } from 'react';
-import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
+import NavItem from '../../NavItem/NavItem.js';
 import logo from '../../images/xSeedLogo.png';
 
 class NavHeader extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { navExpanded: false };
-  }
-  setNavExpanded(expanded) {
-    this.setState({ navExpanded: expanded });
-  }
-  closeNav() {
-    this.setState({ navExpanded: false });
+  handleClick(e) {
+    if (this.props.disabled) {
+      e.preventDefault();
+    } else {
+      if (this.props.onSelect) {
+        this.props.onSelect(this.props.eventKey, e);
+      }      
+    }
   }
 
   render() {
     return (      
-      <Navbar  collapseOnSelect>
+      <Navbar collapseOnSelect={true} >
         <Navbar.Header>
           <Navbar.Brand >
             <a href="#home">
               <img className="navBrandImage" alt="Xseed logo" src={logo} draggable="false" />
             </a>
           </Navbar.Brand>
-          <Navbar.Toggle />
+          <Navbar.Toggle>
+            <span> sad</span>
+          </Navbar.Toggle>
         </Navbar.Header>
-        <Navbar.Collapse>      
+        <Navbar.Collapse  >      
           <Nav pullRight>
-            <li>
-              <NavItem href="#home">
-                <span className="navItem">Home</span>
-              </NavItem>
-            </li>
-            <li >
-              <a href="#hww">
+            <NavItem href="#home" >
+              <span className="navItem">Home</span> 
+            </NavItem>
+            
+            <NavItem href="#hww">
                 <span className="navItem">How we work</span> 
-              </a>
-            </li>
-            <li >
-              <a href="#portfolio">
+            </NavItem>
+            <NavItem href="#portfolio">
                 <span className="navItem">Portfolio</span>
-              </a>
-            </li>
-            <li >
-              <a href="#team">
+            </NavItem>
+            <NavItem href="#team">
                 <span className="navItem">Team</span> 
-              </a>
-            </li>
-            <li >
-              <a href="#contact">
+            </NavItem>
+            <NavItem href="#contact">
                 <span className="navItem">Contact us</span> 
-              </a>
-            </li>
+            </NavItem>
           </Nav>
         </Navbar.Collapse>
       </Navbar>

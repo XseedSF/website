b247554ea7980c5975dab65c3c04b842263f17ef
Diego Cáceres <dcaceres@xseed.com.uy>
2017-05-04 17:01:49 -0300

    added mail server

--- react-ui/src/components/header/NavHeader.js
+++ react-ui/src/components/header/NavHeader.js
@@ -1,47 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import logo from '../../images/xSeedLogo.png';
 
-const NavHeader = () => (
-  <Navbar collapseOnSelect className="navbar">
-    <Navbar.Header>
-      <Navbar.Brand >
-        <a href="#home">
-          <img className="navBrandImage" alt="Xseed logo" src={logo} draggable="false" />
-        </a>
-      </Navbar.Brand>
-      <Navbar.Toggle />
-    </Navbar.Header>
-    <Navbar.Collapse>      
-      <Nav pullRight>
-        <li>
-          <a href="#home">
-            <span className="navItem">Home</span>
-          </a>
-        </li>
-        <li >
-          <a href="#hww">
-            <span className="navItem">How we work</span> 
-          </a>
-        </li>
-        <li >
-          <a href="#portfolio">
-            <span className="navItem">Portfolio</span>
-          </a>
-        </li>
-        <li >
-          <a href="#team">
-            <span className="navItem">Team</span> 
-          </a>
-        </li>
-        <li >
-          <a href="#contact">
-            <span className="navItem">Contact us</span> 
-          </a>
-        </li>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
-);
+class NavHeader extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { navExpanded: false };
+  }
+  setNavExpanded(expanded) {
+    this.setState({ navExpanded: expanded });
+  }
+  closeNav() {
+    this.setState({ navExpanded: false });
+  }
+
+  render() {
+    return (      
+      <Navbar  collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand >
+            <a href="#home">
+              <img className="navBrandImage" alt="Xseed logo" src={logo} draggable="false" />
+            </a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>      
+          <Nav pullRight>
+            <li>
+              <NavItem href="#home">
+                <span className="navItem">Home</span>
+              </NavItem>
+            </li>
+            <li >
+              <a href="#hww">
+                <span className="navItem">How we work</span> 
+              </a>
+            </li>
+            <li >
+              <a href="#portfolio">
+                <span className="navItem">Portfolio</span>
+              </a>
+            </li>
+            <li >
+              <a href="#team">
+                <span className="navItem">Team</span> 
+              </a>
+            </li>
+            <li >
+              <a href="#contact">
+                <span className="navItem">Contact us</span> 
+              </a>
+            </li>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    )
+  }
+};
 
 export default NavHeader;
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+var nodemailer = require("nodemailer");
 const path = require('path');
 
 const app = express();
@@ -6,6 +7,21 @@ const PORT = process.env.PORT || 5000;
 
 // Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+
+/*
+    Here we are configuring our SMTP Server details.
+    STMP is mail server which is responsible for sending and recieving email.
+*/
+var smtpTransport = nodemailer.createTransport({
+  service: "gmail",
+  host: "smtp.gmail.com",
+  auth: {
+    user: "website@xseed.com.uy",
+    pass: "websiteP@ssword2016"
+  }
+});
+
+/*------------------SMTP Over-----------------------------*/
 
 // Answer API requests.
 app.get('/api', function (req, res) {

9bcd5b89d992c45b959f05e310fa861e8a8bf153
Diego Cáceres <dcaceres@xseed.com.uy>
2017-05-10 11:08:42 -0300

    fix css

--- react-ui/src/components/Footer.js
+++ react-ui/src/components/Footer.js
@@ -8,34 +8,36 @@ const Footer = (props) => {
 	const year = new Date().getFullYear();
 	return ( 
 		<div className="footer">
-			<div className="footer-xseed">
-				<p> © {year} XseedIT Solutions </p>
-			</div>
-			<div className="footer-medias">				
-				<a href="https://www.facebook.com/xseeditsolutions/" target="_blank" >
-					<img className="f-icon-img" alt="facebook" src={facebook} draggable="false" /> 					
-				</a>
-                
-                <a href="https://www.instagram.com/xseedsf/" target="_blank" >
-					<img className="f-icon-img" alt="instagram" src={instagram} draggable="false" /> 					
-				</a>
+			<div className="container"> 
+				<div className="footer-xseed">
+					<p> © {year} XseedIT Solutions </p>
+				</div>
+				<div className="footer-medias">				
+					<a href="https://www.facebook.com/xseeditsolutions/" target="_blank" >
+						<img className="f-icon-img" alt="facebook" src={facebook} draggable="false" /> 					
+					</a>
+	                
+	                <a href="https://www.instagram.com/xseedsf/" target="_blank" >
+						<img className="f-icon-img" alt="instagram" src={instagram} draggable="false" /> 					
+					</a>
 
-				<a href="https://twitter.com/XseedSF/" target="_blank" >
-					<img className="f-icon-img" alt="twitter" src={twitter} draggable="false" /> 					
-				</a>
-			</div>
-			<div className="footer-medias-mobile"> 
-				<a className="footer-media-link" href="https://www.facebook.com/xseeditsolutions/" target="_blank" >					
-					<span className="footer-media-mobile"> Facebook </span>
-				</a>
-                -
-                <a className="footer-media-link" href="https://www.instagram.com/xseedsf/" target="_blank" >					
-					<span className="footer-media-mobile"> Instagram </span>
-				</a>
-				-
-				<a className="footer-media-link" href="https://twitter.com/XseedSF/" target="_blank" >					
-					<span className="footer-media-mobile"> Twitter </span>
-				</a>
+					<a href="https://twitter.com/XseedSF/" target="_blank" >
+						<img className="f-icon-img" alt="twitter" src={twitter} draggable="false" /> 					
+					</a>
+				</div>
+				<div className="footer-medias-mobile"> 
+					<a className="footer-media-link" href="https://www.facebook.com/xseeditsolutions/" target="_blank" >					
+						<span className="footer-media-mobile"> Facebook </span>
+					</a>
+	                -
+	                <a className="footer-media-link" href="https://www.instagram.com/xseedsf/" target="_blank" >					
+						<span className="footer-media-mobile"> Instagram </span>
+					</a>
+					-
+					<a className="footer-media-link" href="https://twitter.com/XseedSF/" target="_blank" >					
+						<span className="footer-media-mobile"> Twitter </span>
+					</a>
+				</div>
 			</div>
 		</div>
 	)

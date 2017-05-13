743228c467e7f87f771b1219f6c19fc3870b7a78
Diego Cáceres <dado90caceres@gmail.com>
2017-05-13 15:00:38 -0300

    Fixes

--- react-ui/src/components/PortfolioPage/VideoSection.js
+++ react-ui/src/components/PortfolioPage/VideoSection.js
@@ -5,7 +5,7 @@ const VideoSection = (props) => {
     <div className="p-video-section">
       <div className="overlay"></div>
 
-			<video className="video" loop autoPlay muted>
+			<video className="p-video" loop autoPlay muted>
 
 				<source src={props.source} type='video/mp4;codecs="avc1.42E01E, mp4a.40.2"' />
 		    <img  alt="Your browser does not support the <video> tag" title="Your browser does not support the <video> tag" />
